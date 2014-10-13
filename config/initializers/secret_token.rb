2ee330aca04a546405bc4f94985698d37ec17397
Ryan Leichliter <leichliter.ryan@gmail.com>
2014-10-13 18:51:13 -0500

    Nixie water app  start up.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nixie::Application.config.secret_token = '2878e47c04ed1c8cbe36064f5b088dc9e870d32ed007ae0b6800a6aaf178edb54d9dd99b41d8a37a4fd5d0fe9dafd0a8254618d60debc7e989eb24051e66e263'
