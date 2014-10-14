3f1c6916344fca87db351ee6d2cce121d65fe52c
Ryan Leichliter <leichliter.ryan@gmail.com>
2014-10-14 14:20:41 -0500

    fixed some bugs, removed index.html.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Nixie::Application.routes.draw do
   get "logout" => 'sessions#destroy', :as => 'logout'
   get "login" => 'sessions#new', :as => 'login'
   get "signup" => 'users#new', :as => 'signup'
-  get "user" => 'users#show', :as=> 'user'
+  get "user" => 'users#show', :as => 'user'
+  get "users" => 'users#index', :as => 'users'
+  post "users" => 'users#create', :as => 'create_user'
   root :to => 'pages#index'
 end
