5b48783263f2add25f6cb849b08d2c83637bf8a6
Ryan Leichliter <leichliter.ryan@gmail.com>
2014-10-14 12:01:14 -0500

    removed reset_password option.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Nixie::Application.routes.draw do
   get "logout" => 'sessions#destroy', :as => 'logout'
   get "login" => 'sessions#new', :as => 'login'
   get "signup" => 'users#new', :as => 'signup'
+  get "user" => 'users#show', :as=> 'user'
   root :to => 'pages#index'
 end
