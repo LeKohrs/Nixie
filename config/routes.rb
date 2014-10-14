389f57dd5c6db67ca803d481f864f79bb5546d96
Ryan Leichliter <leichliter.ryan@gmail.com>
2014-10-14 16:25:50 -0500

    completed Authentication process. With no Resource routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Nixie::Application.routes.draw do
   get "logout" => 'sessions#destroy', :as => 'logout'
   get "login" => 'sessions#new', :as => 'login'
   get "signup" => 'users#new', :as => 'signup'
+  post "sessions" => 'sessions#create', :as => 'sessions'
   get "user" => 'users#show', :as => 'user'
   get "users" => 'users#index', :as => 'users'
   post "users" => 'users#create', :as => 'create_user'
