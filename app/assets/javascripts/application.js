6b24289ea3354d52ce2a6bdede70423250e68d78
Ryan Leichliter <leichliter.ryan@gmail.com>
2014-10-18 15:26:00 -0500

    Added a box shadow to container div, Added click feature to glasses.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,37 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+$(document).ready(function(){
+
+  $('.glass1').on("click", function(){
+          $('.glass1').children().attr('src','assets/empty_glass.svg');
+  });
+  
+  $('.glass2').on("click", function(){
+          $('.glass2').children().attr('src','assets/empty_glass.svg');
+  });
+  
+  $('.glass3').on("click", function(){
+          $('.glass3').children().attr('src','assets/empty_glass.svg');
+  });
+  
+  $('.glass4').on("click", function(){
+          $('.glass4').children().attr('src','assets/empty_glass.svg');
+  });
+  
+  $('.glass5').on("click", function(){
+          $('.glass5').children().attr('src','assets/empty_glass.svg');
+  });
+  
+  $('.glass6').on("click", function(){
+          $('.glass6').children().attr('src','assets/empty_glass.svg');
+  });
+  
+  $('.glass7').on("click", function(){
+          $('.glass7').children().attr('src','assets/empty_glass.svg');
+  });
+  
+  $('.glass8').on("click", function(){
+          $('.glass8').children().attr('src','assets/empty_glass.svg');
+  });
+});
